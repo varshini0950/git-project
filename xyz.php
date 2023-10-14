4dc94478057c2003607cea8c20d7aae53c3aeb0c
Pragnasri74 <areyprag@iitbhilai.ac.in>
2023-10-14 17:41:29 +0530

    php file

---
+++ xyz.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+  
+</head>
+<body>
+<h1>
+<?php 
+$calories_per_gram = [
+    'rice' => 1.42,
+    'vegcurry' => 1.5,
+    'nonvegcurry' => 2.5,
+    'chapati' => 2.0,
+    'sweets' => 4,
+    'fruits' => 0.5,
+];
+$total_calories = 0;
+
+    $rice = $_POST['rice'];
+    $vegcurry =$_POST['vegcurry'];
+    $nonvegcurry =$_POST['nonvegcurry'];
+    $chapati = $_POST['chapati'];
+    $sweets = $_POST['sweets'];
+    $fruits = $_POST['fruits'];
+       ?>
+</h1>
+</body>
+</html>
