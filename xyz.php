ca320edd62e2aadaace3b9360747fd7e6c1ef60b
mallarapuhemavarshini <mallarapuhemavarshini@gmail.com>
2023-10-14 10:44:21 +0530

    adding php

---
+++ xyz.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title> Nutrition Result</title>
+</head>
+<body>
+<h1>
+<?php
+$a=$_POST['inp1'];
+$b=$_POST['inp2'];
+$c= $b * $b;
+$d= $a / $c;
+echo ("<p style='font-size:30px'> BMI: $d</br></p>");
+?>
+</h1>
+</body>                     
+</html>                                          
