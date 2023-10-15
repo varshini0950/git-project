7f3266e77e66aa1850312b2184878a7bef70919c
kakivenk <kakivenk@iitbhilai.ac.in>
2023-10-15 08:44:22 +0530

    commit13

--- xyz.php
+++ xyz.php
@@ -12,3 +12,9 @@ $c= $b * $b;
 $d= $a / $c;
 $e=$_POST['exercise'];
 $f=$_POST['inp3'];
+if(isset($_POST['gender'])){
+    $gender = $_POST['gender'];
+echo ("<p style='font-size:30px'>Gender: $gender</p>");}
+else{
+echo ("<p style='font-size:30px'>Gender not selected</p>");
+}
