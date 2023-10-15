163d6a6ff76d5fab87a47a015208759be819c886
mallarapuhemavarshini <mallarapuhemavarshini@gmail.com>
2023-10-15 08:47:11 +0530

    changes in php

--- xyz.php
+++ xyz.php
@@ -5,6 +5,7 @@
 <body>
 <h1>
 <?php
+
 $e=$_POST['exercise'];
 if($e =="cycling")
 {
@@ -19,6 +20,7 @@ echo ("<p style='font-size:30px'>calories burnt:$i cal</br></p>");
 }
 $diff = $total_calories -$i ;
                     echo ("<p style='font-size:30px'>  Net Calorie = $diff cal</br> </p>");
+
 
 ?>
 </h1>
